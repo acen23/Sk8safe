a8b382515396731614eda2226c9b335c5e578ad8
Andrew Cen <acen@ucsd.edu>
2023-11-14 22:37:33 -0800

    created new app project

---
+++ Sk8safe/Sk8safe/Sk8safeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sk8safeApp.swift
+//  Sk8safe
+//
+//  Created by Andrew Cen on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Sk8safeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
